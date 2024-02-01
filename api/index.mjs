8648c8996b28286f5f525b6cfe26988f6ec30d8f
alex <marketdoski@gmail.com>
2024-02-01 17:30:09 +0200

    del corsOptions with headers

--- api/index.mjs
+++ api/index.mjs
@@ -10,23 +10,10 @@ import {
 const PORT = 5000
 
 const app = express()
-
 app.use(bodyParser.json())
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://time-app-docker-frontend.vercel.app');
-  res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization');
-  res.header('Access-Control-Allow-Credentials', true);
-  next();
-});
-
 const corsOptions = {
   origin: 'https://time-app-docker-frontend.vercel.app',
   optionsSuccessStatus: 200,
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  credentials: true,
-  preflightContinue: false,
-  allowedHeaders: 'Content-Type,Authorization',
 }
 app.use(cors(corsOptions))
 
